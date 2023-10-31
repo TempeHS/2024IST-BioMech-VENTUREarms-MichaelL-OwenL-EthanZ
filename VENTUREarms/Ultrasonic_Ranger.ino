af7d8d00b72a77bcf95c1cf3c28a6dfaa79caf34
MLiondis <Michael.liondis@education.nsw.gov.au>
2023-10-31 11:53:41 +1100

    add code

--- VENTUREarms/Ultrasonic_Ranger.ino
+++ VENTUREarms/Ultrasonic_Ranger.ino
@@ -10,17 +10,17 @@
 // This is a different Ultrasonic Library to the one we used in the bootcamp becuase we are using a Seead grove sensor not a generic 4 pin Ultrasonic sensor
 #include "Ultrasonic.h"
 
-Ultrasonic myUltrasonicSensor(5);
+Ultrasonic myUltrasonicSensor(5);  // attach to pin (5)
 void setup()
 {
- Serial.begin(9600);
+ Serial.begin(9600);            
 }
 void loop()
 {
   long RangeInCentimeters;
 
   RangeInCentimeters = myUltrasonicSensor.MeasureInCentimeters(); // two measurements should keep an interval
-  Serial.print(RangeInCentimeters);//0~400cm
+  Serial.print(RangeInCentimeters);//0~10cm
   Serial.println(" cm");
-  delay(250);
+  delay(500);
 }
